ba4a70a9d8a5647c60b404f813df7858071402e4
No One <franklinquintin82@gmail.com>
2020-03-15 13:15:46 -0500

    new syntax

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-var express = require ('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
 var app = express();
-var PORT = process.env.PORT || 8080;
-app.use(bodyParser.urlencoded({ extended: true }));
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(bodyParser.text());
 
+//in hot restaurants we saved data in server.js
+//in hot restaurants we defined routes in server.js
+//now we save data in friends.js
+//friends.js will be used in routes js files
+//and we need to import those routes files
+var apiroutes = require("./app/routing/apiroutes.js");
+var htmlroutes = require("./app/routing/htmlroutes.js");
 
-require('./app/routing/apiRoutes')(app);
-require('./app/routing/htmlRoutes')(app);
+app.use(apiroutes);
+app.use(htmlroutes);
 
-
-
-
-
+//listening
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
